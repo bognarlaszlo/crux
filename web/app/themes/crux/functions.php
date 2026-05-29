b2439c9bb69b4f810d4225f07cef513dd76972b3
Bognár László <bognar.laszlo@ingatlan.com>
2026-05-29 13:51:36 +0200

    feat: rename setup.php to wordpress.php

--- web/app/themes/crux/functions.php
+++ web/app/themes/crux/functions.php
@@ -2,5 +2,5 @@
 
 require_once dirname(__DIR__, 4) . '/vendor/autoload.php';
 
-require_once __DIR__ . '/src/setup.php';
+require_once __DIR__ . '/src/wordpress.php';
 require_once __DIR__ . '/src/timber.php';
