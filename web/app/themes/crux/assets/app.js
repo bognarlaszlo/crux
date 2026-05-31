1891930b643621022eb45b380aaac1442cf88b59
Bognár László <bognar.laszlo@ingatlan.com>
2026-05-31 10:49:49 +0200

    fix: fix malformed import in app.js and malformed controllers.json

--- web/app/themes/crux/assets/app.js
+++ web/app/themes/crux/assets/app.js
@@ -1,6 +1,6 @@
 import './styles/app.css';
 
-import { registerControllers } from 'stimulus-factory';
+import { registerControllers } from './stimulus-factory';
 
 registerControllers(require.context(
   './controllers', true, /\.[jt]sx?$/
