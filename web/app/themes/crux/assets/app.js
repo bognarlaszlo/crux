cca325f0ad7875803256c3281cefe7413bf4e106
Bognár László <bognar.laszlo@ingatlan.com>
2026-06-02 14:57:02 +0200

    fix: simplify stimulus initialization

--- web/app/themes/crux/assets/app.js
+++ web/app/themes/crux/assets/app.js
@@ -1,7 +1,4 @@
 import './styles/app.scss';
 
-import { registerControllers } from './stimulus-factory';
-
-registerControllers(require.context(
-  './controllers', true, /\.[jt]sx?$/
-));
+// Initialize Stimulus App
+import './stimulus';
