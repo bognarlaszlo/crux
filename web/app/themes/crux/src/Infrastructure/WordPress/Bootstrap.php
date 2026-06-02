3a0491b458ce6e2a00ff1e24be99f4774bad5a8f
Bognár László <bognar.laszlo@ingatlan.com>
2026-06-02 14:36:20 +0200

    feat: admin features update - Add custom menu item - Remove Gutenberg support - Allow pages and pages to have editor

--- web/app/themes/crux/src/Infrastructure/WordPress/Bootstrap.php
+++ web/app/themes/crux/src/Infrastructure/WordPress/Bootstrap.php
@@ -6,28 +6,40 @@ class Bootstrap
 {
     public static function init(): void
     {
-        add_action('admin_menu', [static::class, 'removeAdminMenuPages']);
+        add_action('admin_menu', [static::class, 'editAdminMenu']);
         add_action('admin_init', [static::class, 'removeAdminFeatures']);
+        add_filter('comments_array', '__return_empty_array', 10, 2);
         add_filter('comments_open', '__return_false', 20);
         add_filter('pings_open', '__return_false', 20);
-        add_filter('comments_array', '__return_empty_array', 10, 2);
+        add_filter('use_block_editor_for_post_type', '__return_false');
         add_action('wp_before_admin_bar_render', [static::class, 'removeAdminBarComments']);
     }
 
-    public static function removeAdminMenuPages(): void
+    public static function editAdminMenu(): void
     {
         remove_menu_page('edit.php');
-        remove_menu_page('edit.php?post_type=page');
         remove_menu_page('edit-comments.php');
         remove_menu_page('themes.php');
         remove_menu_page('plugins.php');
         remove_menu_page('edit.php?post_type=acf-field-group');
+        remove_menu_page('users.php');
+
+        add_menu_page(
+            __('Menük', 'crux'),
+            __('Menük', 'crux'),
+            'edit_theme_options',
+            'nav-menus.php',
+            '',
+            'dashicons-menu',
+            61
+        );
     }
 
     public static function removeAdminFeatures(): void
     {
+        remove_post_type_support('post', 'editor');
+
         foreach (['post', 'page'] as $postType) {
-            remove_post_type_support($postType, 'editor');
             remove_post_type_support($postType, 'comments');
             remove_post_type_support($postType, 'trackbacks');
         }
