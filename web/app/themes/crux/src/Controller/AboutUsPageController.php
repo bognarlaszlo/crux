51323a27bbceae3556d23bec6d46297a319e642f
Bognár László <bognar.laszlo@ingatlan.com>
2026-07-02 16:12:51 +0200

    feat: added board post types to about us page

--- web/app/themes/crux/src/Controller/AboutUsPageController.php
+++ web/app/themes/crux/src/Controller/AboutUsPageController.php
@@ -3,13 +3,25 @@
 namespace Crux\Controller;
 
 use Crux\Content\AboutPageContent;
+use Timber\PostCollectionInterface;
+use Timber\Timber;
 
 final class AboutUsPageController extends AbstractController
 {
     public static function index(): void
     {
         self::render('templates/about-us-page.twig', [
-            AboutPageContent::all()
+            AboutPageContent::all(),
+            [
+                'board' => self::board(),
+            ]
+        ]);
+    }
+
+    private static function board(): ?PostCollectionInterface
+    {
+        return Timber::get_posts([
+            'post_type' => 'board',
         ]);
     }
 }
